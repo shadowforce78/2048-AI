0fc7325e802dbe7e714e94aa7af9eb7a21612050
shadowforce78 <planque.adam@gmail.com>
2023-05-15 02:05:18 +0200

    added agent

--- agent.py
+++ agent.py
@@ -1,8 +1,7 @@
 import pygame
-import game
+from game import *
 import tensorflow as tf
 import numpy as np
-import random
 
 MOUVES = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
 
@@ -21,11 +20,11 @@ def get_data():
     X = []
     y = []
     for i in range(1000):
-        grid = game.init_grid()
+        grid = init_grid()
         while True:
             prev_grid = [row[:] for row in grid]
             key = np.random.choice(MOUVES)
-            game.update_grid(grid, key)
+            update_grid(grid, key)
             if grid != prev_grid:
                 X.append(grid)
                 y.append(MOUVES.index(key))
@@ -34,14 +33,21 @@ def get_data():
     y = tf.keras.utils.to_categorical(y)
     return X, y
 
+def update_data(grid, key, X, y):
+    prev_grid = [row[:] for row in grid]
+    update_grid(grid, key)
+    if grid != prev_grid:
+        X.append(grid)
+        y.append(MOUVES.index(key))
 
 def train_model(model, X, y):
     model.fit(X, y, epochs=10)
 
-def play_game(model):
-    grid = game.init_grid()
-    game.add_new_tile(grid)
-    game.draw_grid(grid)
+# Appel de la fonction principale pour commencer le jeu
+def agent_play_game(model, X, y):
+    grid = init_grid()
+    add_new_tile(grid)
+    draw_grid(grid)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -51,35 +57,21 @@ def play_game(model):
                 if event.key == pygame.K_q:
                     pygame.quit()
                     return
-                elif event.key in MOUVES:
-                    prev_grid = [row[:] for row in grid]
-                    game.update_grid(grid, event.key)
-                    if grid != prev_grid:
-                        game.add_new_tile(grid)
-                    game.draw_grid(grid)
-                    if game.is_game_over(grid):
-                        pygame.quit()
-                        return
-                    X = np.array([grid])
-                    y = model.predict(X)
-                    key = MOUVES[np.argmax(y)]
-                    prev_grid = [row[:] for row in grid]
-                    game.update_grid(grid, key)
-                    if grid != prev_grid:
-                        game.add_new_tile(grid)
-                    game.draw_grid(grid)
-                    if game.is_game_over(grid):
-                        pygame.quit()
-                        return
+        best_move = get_best_move(grid, model)
+        if best_move:
+            prev_grid = [row[:] for row in grid]
+            update_grid(grid, best_move)
+            update_data(grid, best_move, X, y)
+            if grid != prev_grid:
+                add_new_tile(grid)
+            draw_grid(grid)
+            if is_game_over(grid):
+                grid = init_grid()
+                add_new_tile(grid)
+                draw_grid(grid)
 
-def main():
-    pygame.init()
-    window = pygame.display.set_mode((game.WINDOW_SIZE, game.WINDOW_SIZE))
-    pygame.display.set_caption("2048")
+if __name__ == '__main__':
     model = get_model()
     X, y = get_data()
     train_model(model, X, y)
-    play_game(model)
-
-if __name__ == '__main__':
-    main()
+    agent_play_game(model, X, y)
--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import pygame
 import random
-from agent import *
 import tensorflow as tf
 import numpy as np
+import agent
 
 # Définition des constantes
 WINDOW_SIZE = 400
@@ -21,7 +21,7 @@ CELL_COLORS = {
     256: (237, 204, 97),
     512: (237, 200, 80),
     1024: (237, 197, 63),
-    2048: (237, 194, 46)
+    2048: (237, 194, 46),
 }
 
 # Initialisation de Pygame
@@ -29,17 +29,22 @@ pygame.init()
 window = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
 pygame.display.set_caption("2048")
 
+
 # Fonction pour initialiser la grille
 def init_grid():
     grid = [[0] * GRID_SIZE for _ in range(GRID_SIZE)]
     return grid
 
+
 # Fonction pour ajouter une nouvelle tuile à la grille
 def add_new_tile(grid):
-    empty_cells = [(i, j) for i in range(GRID_SIZE) for j in range(GRID_SIZE) if grid[i][j] == 0]
+    empty_cells = [
+        (i, j) for i in range(GRID_SIZE) for j in range(GRID_SIZE) if grid[i][j] == 0
+    ]
     if empty_cells:
         row, col = random.choice(empty_cells)
         grid[row][col] = random.choice([2, 4])
+
 
 # Fonction pour afficher la grille
 def draw_grid(grid):
@@ -48,13 +53,23 @@ def draw_grid(grid):
         for col in range(GRID_SIZE):
             cell_value = grid[row][col]
             cell_color = CELL_COLORS.get(cell_value, (255, 255, 255))
-            pygame.draw.rect(window, cell_color, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            pygame.draw.rect(
+                window,
+                cell_color,
+                (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE),
+            )
             if cell_value != 0:
                 font = pygame.font.Font(None, 50)
                 text = font.render(str(cell_value), True, (0, 0, 0))
-                text_rect = text.get_rect(center=(col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2))
+                text_rect = text.get_rect(
+                    center=(
+                        col * CELL_SIZE + CELL_SIZE // 2,
+                        row * CELL_SIZE + CELL_SIZE // 2,
+                    )
+                )
                 window.blit(text, text_rect)
     pygame.display.update()
+
 
 # Fonction pour déplacer les tuiles vers la gauche
 def move_left(grid):
@@ -73,15 +88,16 @@ def move_left(grid):
                     grid[row][col] = 0
                     merged_row = True  # Indiquer la fusion dans la ligne
 
+
 # Fonction pour inverser la grille
 def transpose_grid(grid):
     grid[:] = [list(row) for row in zip(*grid)]
+
 
 # Fonction pour inverser les lignes de la grille
 def reverse_grid(grid):
     for row in grid:
         row.reverse()
-
 
 
 # Fonction pour mettre à jour la grille en fonction de la touche pressée
@@ -107,7 +123,6 @@ def update_grid(grid, key):
         transpose_grid(grid)
 
 
-
 # Fonction pour vérifier si aucun mouvement n'est possible
 def is_game_over(grid):
     for row in range(GRID_SIZE):
@@ -119,6 +134,7 @@ def is_game_over(grid):
             if row < GRID_SIZE - 1 and grid[row][col] == grid[row + 1][col]:
                 return False
     return True
+
 
 # Fonction principale pour jouer au jeu
 def play_game():
@@ -134,8 +150,15 @@ def play_game():
                 if event.key == pygame.K_q:
                     pygame.quit()
                     return
-                elif event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
-                    prev_grid = [row[:] for row in grid]  # Copie de la grille précédente
+                elif event.key in [
+                    pygame.K_LEFT,
+                    pygame.K_RIGHT,
+                    pygame.K_UP,
+                    pygame.K_DOWN,
+                ]:
+                    prev_grid = [
+                        row[:] for row in grid
+                    ]  # Copie de la grille précédente
                     update_grid(grid, event.key)
                     if grid != prev_grid:
                         add_new_tile(grid)
@@ -146,6 +169,66 @@ def play_game():
                         draw_grid(grid)
 
 
+# Création du modèle
+def get_empty_cells(grid):
+    empty_cells = []
+    for row in range(GRID_SIZE):
+        for col in range(GRID_SIZE):
+            if grid[row][col] == 0:
+                empty_cells.append((row, col))
+    return empty_cells
+
+
+# Fonction pour obtenir le meilleur mouvement
+def get_best_move(grid, model):
+    scores = []
+    for move in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
+        prev_grid = [row[:] for row in grid]
+        update_grid(grid, move)
+        if not np.array_equal(grid, prev_grid):
+            X = np.array([grid])
+            y = model.predict(X)
+            scores.append((move, y[0][0]))
+        grid = prev_grid
+    if scores:
+        scores.sort(key=lambda x: x[1], reverse=True)
+        best_moves = [move for move, score in scores if score == scores[0][1]]
+        best_move = random.choice(best_moves)
+        return best_move
+    return None
+
+
+
+
+
+
+def agent_play_game(model):
+    grid = init_grid()
+    add_new_tile(grid)
+    draw_grid(grid)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    return
+        best_move = get_best_move(grid, model)
+        if best_move:
+            prev_grid = [row[:] for row in grid]
+            update_grid(grid, best_move)
+            agent.update_data(prev_grid, best_move)
+            if grid != prev_grid:
+                add_new_tile(grid)
+            draw_grid(grid)
+            if is_game_over(grid):
+                grid = init_grid()
+                add_new_tile(grid)
+            draw_grid(grid)
+
 
 # Appel de la fonction principale pour commencer le jeu
-#play_game()
+model = agent.get_model()
+agent_play_game(model)
